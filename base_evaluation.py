31914ffefd66ecfc4f07da8bb92295f47a24d675
Magneto <mandy.khadka10@gmail.com>
2026-04-25 13:37:17 +0200

    test fp16

--- base_evaluation.py
+++ base_evaluation.py
@@ -30,7 +30,7 @@ from src.judge import Judge
 DATASET_PATH = "data/HarmBench.csv"
 N_GOALS = None
 RANDOM_SEED = 42
-PRECISION = "int8"
+PRECISION = "fp16"
 
 CHECKPOINT_PATH = f"results/checkpoint_base_{PRECISION}.json"
 
